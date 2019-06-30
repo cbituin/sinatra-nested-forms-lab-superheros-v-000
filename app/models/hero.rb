6e08762d1eaf5fbc6c7ffdef218733bcc5e992fc
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-06-30 05:09:27 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -5,7 +5,15 @@ class Hero
   HEROES = []
   
   def initialize(args)
+    @name = args[:name]
+    @power = args[:power]
+    @bio = args[:bio]
     
+    HEROES << self
+  end
+  
+  def self.all
+    HEROES
   end
   
 end
